ef758fbda046bc06c54be468b56537c790bf0ddd
SHASHANK-CODING <75520457+SHASHANK-CODING@users.noreply.github.com>
2023-03-18 21:42:02 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,37 @@
 
 ## Write a short comment describing this function
 
+library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
+  int <- NULL
+  set<-function(y){
+                    x<<-y
+                    int<<-NULL
+                    }
 
+  get<-function()x
+  setinv<-function(inverse)int<<-inverse
+  getinv<-function() { 
+                    inver<-ginv(x)
+                    inver%*%x
+                    }
+  list(set = set,get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
-
-
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+  {
+  inv<-x$getinv()
+  if(!is.null(inv)) {
+                    message("getting catched data!")
+                    return(inv)
+  }
+  data<-x$get()
+  int<-solve(data,...)
+  x$setinv(inv)
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
+
